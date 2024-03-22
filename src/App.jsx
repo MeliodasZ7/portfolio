eab6d908c64dee593788eab5684b4c9403c5a556
unknown <warat.russsameenopsawet@gmail.com>
2024-03-23 03:31:48 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { ImageProfile } from './components/ImageProfile'
 import { Title } from './components/Title'
-import moment from 'moment'
-import profile from './assets/react.svg'
+import moment from 'moment';
+import profile from './assets/profile.png'
+import axios from 'axios';
+
+const endpoint = 'https://sheetdb.io/api/v1/xy9jp2v8n33zt'
+
 function App() {
   const [hide, setHide] = useState(false)
+  const [data, setData] = useState([])
+  
+  const callApi = async () => {
+    const d = await axios.get(endpoint).then((response) => {
+      console.log('response', response)
+      const data = response.data
+      setData(data)
+    })
+    console.log('success', d)
+  }
+
+  useEffect(() =>{
+    callApi()
+  },[])
 
   return (
     <main id="main">
@@ -37,11 +55,15 @@ function App() {
 
       <div className="rightPort">
         <Title title="Profile">
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi quia dignissimos corporis sequi aspernatur unde non ullam voluptas, eveniet dicta ipsum? Error rem dolorem sit facilis molestiae at sequi commodi!</p>
+          <p>ผมเป็นคนที่ชอบลองผิดลองถูก ชอบค้นหาและเรียนรู้สิ่งใหม่ๆ พร้อมที่จะพัฒนาตัวเองได้เสมอ สามารถทำงานเป็นทีมได้ เวลาที่คิดจะทำอะไรแล้วจะทำจนกว่าจะสำเร็จ ถ้าไม่พอใจก็จะลงมือทำใหม่จนกว่าจะออกมาดีที่สุด</p>
         </Title>
 
         <Title title="Work Experience">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquid debitis nulla tempora velit eligendi necessitatibus eaque, corrupti ipsum sed vitae laboriosam? Quaerat ea qui asperiores veritatis perferendis quasi impedit!</p>
+        <h3>สร้าง AI ทำนายความเสี่ยงของโรคหัวใจ</h3>
+          <p>• ทำการนำค่าต่างๆของผู้ป่วยที่เป็นหรือไม่เป็นโรคหัวใจมาเทรน AI โดยใช้โครงข่ายประสาทเทียม Decition Tree</p>
+          <p>• สร้างหน้าเว็บไซต์เพื่อให้ผู้ใช้งานกรอกค่าต่างๆ ตัวอย่างเช่น    อัตราการเต้นหัวใจ, ความดันโลหิตต่ำ-สูง เป็นต้น จากนั้นกดปุ่มตรวจสอบ จากนั้น AI จะทำการทำนายว่ามีโอกาศที่จะเป็นโรคหัวใจหรือไม่</p>
+          <h3>โปรเจกต์เกม Guardians of Realms</h3>
+          <p>• สร้างเกม Guardians of Realms ขึ้นมา เป็นเกมแนว A-RPG ตะลุยผ่านด่านไปเรื่อยๆ มีระบบต่างๆ เช่น ร้านค้าซื้อตัวละครกับอาวุธ และเชื่อมต่อกับ Database Phpmyadmin โดยจะบันทึกข้อมูลต่างๆของผู้เล่น และจะมีโหมด Score เพื่อให้ผู้เล่นได้เก็บ Score แสดงอันดับใน Ranking</p>
         </Title>
 
         <Title title="Skills">
@@ -51,6 +73,11 @@ function App() {
           <p>Vue.js</p>
           <p>Git</p>
         </Title>
+
+        {/* <div>
+        เพื่อนในชั้นเรียน<br />
+        {data.map((record) => <div key={record.code}>{record.code}{record.name}</div>)}
+      </div> */}
 
       </div>
     </main>
